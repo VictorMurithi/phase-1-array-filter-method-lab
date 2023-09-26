4cb3d8e93c564d8b1c57feb3d151db643faa78fe
victor <victor.murithi@student.moringaschool.com>
2023-09-26 12:05:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers,letter){
+    return drivers.filter(driver => driver.startsWith(letter))
+}
+function matchName(drivers,name){
+    return drivers.filter(driver=>driver.name === name)
+}
